0edd1bbeea681d2f47237dd2535ef969bdc88f0a
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-03-14 00:55:37 -0300

    .

--- src/app/components/dashboard/navbar/navbar.component.ts
+++ src/app/components/dashboard/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FormModalComponent } from '../form-modal/form-modal.component';
 import { LoginService } from 'src/app/services/login.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 
 @Component({
@@ -12,11 +13,19 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(public dialog: MatDialog, private loginServicio: LoginService){
+  constructor(public dialog: MatDialog,
+    private loginServicio: LoginService,
+    private usuarioService:UsuarioService){
 
+  }
+  ngOnInit() {
+    this.usuarioService.getUsuario().subscribe(res=>{
+      this.rolAdmin = res.user.role=='admin'
+    })
   }
   opened!: boolean;
   shouldRun = false;
+  rolAdmin= false;
 
   openModal() {
     const dialogRef = this.dialog.open(FormModalComponent, {
